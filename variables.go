ae090fbc0a23964e4f4f6a985eedf844c8792595
Pooja Tanneru <tannerupooja01@gmail.com>
2025-07-18 13:35:49 -0500

    variables in go

---
+++ variables.go
@@ -0,0 +1,13 @@
+package main 
+import "fmt"
+
+func main(){
+	name := "Pooja"
+	age := 23
+	height := 5.4
+
+	fmt.Println("Name = ", name)
+	fmt.Println("Age = ", age)
+	fmt.Println("Height = ", height)
+
+}
